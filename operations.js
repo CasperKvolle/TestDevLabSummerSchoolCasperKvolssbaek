e912e61b90b7ebdf5294634bbc5ba1d4822ebdff
Casper Kvolle <Casper@kvolle.com>
2024-07-26 10:57:35 +0300

    new file added

---
+++ operations.js
@@ -0,0 +1,20 @@
+
+
+let number = '1'
+let anotherNumber = '2'
+
+console.log(number > anotherNumber)
+console.log(number >= anotherNumber)
+console.log(number < anotherNumber)
+console.log(number <= anotherNumber)
+console.log(number == anotherNumber) // equility
+console.log(number === anotherNumber)// strick equility
+console.log(number && anotherNumber) //  AND
+console.log(number || anotherNumber) // OR
+console.log(!number && !anotherNumber ) // NOT
+
+
+let isSunny = false;
+const wheatherMesagge = isSunny ? 'Grab sunglasses!' : ' Stay home';
+console.log(wheatherMesagge)
+
